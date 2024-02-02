fcaf55f2e5d9630728903cd68031813efcdb99b2
YULIA VASILENKO <yuliav@devart.com>
2024-02-02 16:12:53 +0200

    initial commit

---
+++ Tables/dbo.Customers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customers] (
+  [CustomerID] [int] NOT NULL,
+  [FirstName] [varchar](50) NULL,
+  [LastName] [varchar](50) NULL,
+  [Email] [varchar](100) NULL,
+  [Phone] [varchar](20) NULL,
+  PRIMARY KEY CLUSTERED ([CustomerID])
+)
+ON [PRIMARY]
+GO
---
+++ Tables/dbo.Orders.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Orders] (
+  [OrderID] [int] NOT NULL,
+  [CustomerID] [int] NULL,
+  [OrderDate] [date] NULL,
+  [TotalAmount] [decimal](10, 2) NULL,
+  PRIMARY KEY CLUSTERED ([OrderID])
+)
+ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[Orders] WITH NOCHECK
+  ADD FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID])
+GO
