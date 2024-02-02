40d8c70bd97492a8f4f7d725f19a3669862503f3
YULIA VASILENKO <yuliav@devart.com>
2024-02-02 16:32:52 +0200

    add products old

---
+++ Tables/dbo.Products_old.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Products_old] (
+  [ProductID] [int] NOT NULL,
+  [ProductName] [varchar](100) NULL,
+  [Category] [varchar](50) NULL,
+  [Price] [decimal](10, 2) NULL,
+  [StockQuantity] [int] NULL,
+  PRIMARY KEY CLUSTERED ([ProductID])
+)
+ON [PRIMARY]
+GO
